3dff434e194510a7a4e214351ce34b7995b3ff62
John Gaitho <johngaitho05@gmail.com>
2023-11-28 23:46:02 +0300

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,45 @@
-
-# Using Puppet| Install Nginx server, setup and configuration
-
+include ufw
+# Install Nginx web server (w/ Puppet)
 package { 'nginx':
-  ensure => 'installed'
+  package_ensure => installed,
+  service_ensure => 'running',
+}
+
+file{ 'Redirect rule':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=hdZUCjAQaGw permanent;',
 }
 
 file { '/var/www/html/index.html':
   content => 'Hello World',
 }
 
-file_line { 'redirection-301':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
+}
+package {'ufw':
+ensure => installed,
+  }
+
+service {'ufw':
+ensure  => running,
+enable  => true,
+require => Package['ufw'],
+}
+# allow Nginx and SSh
+ufw{'Nginx FULL':
+rule  => 'allow',
+port  => [80,443],
+proto => 'tcp',
+order => 1,
+}
+
+ufw {'SSH':
+rule  => 'allow',
+port  => 22,
+proto => 'tcp',
+order => 2,
 }
