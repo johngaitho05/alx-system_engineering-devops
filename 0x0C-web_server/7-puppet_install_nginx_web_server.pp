96ebc153e5ad465ea0d88a0c6b45a61505539896
John Gaitho <johngaitho05@gmail.com>
2023-12-04 20:47:45 +0300

    update scripts

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,66 @@
-# File:   7-puppet_install_nginx_web_server.pp
-# Author: Alex Orland Arévalo Tribaldos
-# email:  <3915@holbertonschool.com>
+# nginx_install.pp
 
-# Using Puppet| Install Nginx server, setup and configuration
+# Update package lists
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update -y',
+  path    => ['/bin', '/usr/bin'],
+}
+
+# Install nginx web server
 package { 'nginx':
-  ensure => 'installed'
+  ensure  => installed,
+  require => Exec['apt-update'],
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World',
+# Configure nginx on port 80
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => @("EOL"),
+server {
+    listen 80 default_server;
+    server_name _;
+
+    location / {
+        root /usr/share/nginx/html;
+        index index.html;
+    }
+
+    location /custom_404.html {
+        internal;
+        root /usr/share/nginx/html;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=dphzDKZa6xk;
+    }
+
+    error_page 404 /custom_404.html;
+    location = /custom_404.html {
+        root /usr/share/nginx/html;
+        internal;
+    }
+}
+EOL
+  require => Package['nginx'],
 }
 
-file_line { 'redirection-301':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
+# Start nginx service
 service { 'nginx':
   ensure  => running,
+  enable  => true,
   require => Package['nginx'],
+}
+
+# Create index.html
+file { '/usr/share/nginx/html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
+  require => Service['nginx'],
+}
+
+# Create custom_404.html
+file { '/usr/share/nginx/html/custom_404.html':
+  ensure  => present,
+  content => "Ceci n'est pas une page",
+  require => Service['nginx'],
 }
