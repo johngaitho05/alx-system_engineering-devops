de0a31060918223ba7217287dc8ab8b7129a9be2
John Gaitho <johngaitho05@gmail.com>
2024-01-02 18:41:46 +0300

    user api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A REST API implementation to fetch employee info"""
+
+import requests
+import sys
+
+user_id = sys.argv[1]
+user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+todos_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+
+user = requests.get(user_url).json()
+tasks = requests.get(todos_url).json()
+completed_tasks = list(filter(lambda t: t['completed'], tasks))
+
+print("Employee {} is done with tasks({}/{}):"
+      .format(user['name'], len(completed_tasks), len(tasks)))
+
+for task in completed_tasks:
+    print("\t {}".format(task['title']))
