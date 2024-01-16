ee9cb42650f5c355819c991ba0b548372d3bd41c
John Gaitho <johngaitho05@gmail.com>
2024-01-16 22:36:28 +0300

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""
-How many subs?
-"""
+"""Fetch subreddit subs"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Count subreddit subscribers
-    """
-    api_header = {
-        'User-Agent': 'Mozilla/5.0'
-    }
-    api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    api_res = requests.get(api_url, headers=api_header, allow_redirects=False)
-    if api_res.status_code != 200:
+    """Queries Reddit API and returns the number of subscribers
+    to the subreddit"""
+
+    response = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "My-User-Agent"},
+        allow_redirects=False)
+    if response.status_code >= 300:
         return 0
-    api_json = api_res.json()
-    return api_json['data']['subscribers']
+
+    return response.json().get("data").get("subscribers")
