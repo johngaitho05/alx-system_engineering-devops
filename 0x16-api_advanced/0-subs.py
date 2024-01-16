95c549150e61f6ad7e344842efba00130dc258c7
John Gaitho <johngaitho05@gmail.com>
2024-01-16 22:08:49 +0300

    refactor

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,5 +8,7 @@ def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'gritnec/1.0 (by /u/johngaitho)'}
     response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        return 0
     data = response.json() or {}
     return data.get('data', {}).get('subscribers', 0)
