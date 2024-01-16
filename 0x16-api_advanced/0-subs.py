f9023c5f50a909935d15d2f5228c3fd81bb560b4
John Gaitho <johngaitho05@gmail.com>
2024-01-16 23:15:36 +0300

    top 10

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,8 @@ def number_of_subscribers(subreddit):
     """Fetch reddit subs"""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'gritnec'}
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            data = response.json() or {}
-            return data.get('data', {}).get('subscribers', 0)
-        return 0
-    except Exception as e:
-        return 0
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json() or {}
+        return data.get('data', {}).get('subscribers', 0)
+    return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
-
+"""Get top 10 hot posts"""
 import requests
 
 
 def top_ten(subreddit):
-    pass
+    """Top 10 hot posts"""
 
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'User-Agent': 'gritnec'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        posts = response.json().get('data', {}).get('children', [])
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        print(None, response.status_code)
+
+
+top_ten('programming')
