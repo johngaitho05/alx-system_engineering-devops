bb02d3b46e800076f2822f8734acfc1ec75cb922
John Gaitho <johngaitho05@gmail.com>
2024-01-16 22:07:31 +0300

    refactor

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,6 @@ def number_of_subscribers(subreddit):
     """Fetch reddit subs"""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'gritnec/1.0 (by /u/johngaitho)'}
-
-    try:
-        response = requests.get(url, headers=headers)
-        data = response.json()
-        subscribers_count = data['data']['subscribers']
-        return subscribers_count
-    except KeyError:
-        return 0
+    response = requests.get(url, headers=headers)
+    data = response.json() or {}
+    return data.get('data', {}).get('subscribers', 0)
