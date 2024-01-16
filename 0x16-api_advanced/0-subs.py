02cacfefccc2ef7169b818124e05e0f354fff758
John Gaitho <johngaitho05@gmail.com>
2024-01-16 22:00:14 +0300

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,10 @@
-#!/usr/bin/pythpon3
+#!/usr/bin/python3
 """Fetch subreddit subs"""
 import requests
 
 
 def number_of_subscribers(subreddit_name):
+    """Fetch reddit subs"""
     url = f'https://www.reddit.com/r/{subreddit_name}/about.json'
     headers = {'User-Agent': 'gritnec/1.0 (by /u/johngaitho)'}
 
