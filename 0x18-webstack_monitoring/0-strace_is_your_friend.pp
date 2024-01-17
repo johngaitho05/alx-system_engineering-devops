b4960aeaf0bc7875083de0261e2c19d17ae1042a
John Gaitho <johngaitho05@gmail.com>
2024-01-17 22:29:43 +0300

    fix-wordpress

--- 0x18-webstack_monitoring/0-strace_is_your_friend.pp
+++ 0x18-webstack_monitoring/0-strace_is_your_friend.pp
@@ -3,4 +3,4 @@ exec { 'fix-wordpress':
   command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
 /var/www/html/wp-settings.php; service apache2 restart"',
   path    => '/usr/bin:/usr/sbin:/bin'
-}
+
